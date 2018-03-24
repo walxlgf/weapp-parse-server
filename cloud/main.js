6bd395f013105cf0eff499d42a8cd7025f71fbdf
walxlgf <guanghui-li@163.com>
2018-03-24 19:59:52 +0800

    增加注册登录流程方法:weappauth

--- cloud/main.js
+++ cloud/main.js
@@ -3,12 +3,10 @@ const APPID = 'wxc14d0ff891dbbb64';
 const SECRET = '654f6c6559336fa79d13c85e4cb2e080';
 
 
-Parse.Cloud.define('wechatLogin', (req, res) => {
+Parse.Cloud.define('weappauth', (req, res) => {
   // Parse.Cloud.useMasterKey();
   var code = req.params.code;
   console.log(`cloud:wechatLogin:code:${code}`)
-  var openid;
-  var session_key;
   //获取openId 
   Parse.Cloud.httpRequest({
     url: 'https://api.weixin.qq.com/sns/jscode2session',
@@ -22,11 +20,30 @@ Parse.Cloud.define('wechatLogin', (req, res) => {
       grant_type: 'authorization_code',
     }
   }, { useMasterKey: true }).then(function (httpResponse) {
-    openid = httpResponse.data.openid;
-    session_key = httpResponse.data.session_key
+    var openid = httpResponse.data.openid;
     console.log(`cloud:wechatLogin:openid:${openid}`)
-    console.log(`cloud:wechatLogin:session_key:${session_key}`)
-    res.success(httpResponse.data);
+    //判断是否存在此用户
+    var query = new Parse.Query(Parse.User);
+    query.equalTo("username", openid);
+    return query.first();
+  }).then(function (user) {
+    if (user) {
+      return Parse.User.logIn(openid, openid);
+    } else {
+      var user = new Parse.User();
+      user.set("username",openid);
+      user.set("password", openid);
+      user.set("nickName", httpResponse.data.nickName);
+      user.set("gender", httpResponse.data.gender);
+      user.set("language", httpResponse.data.language);
+      user.set("city", httpResponse.data.city);
+      user.set("province", httpResponse.data.province);
+      user.set("country", httpResponse.data.country);
+      user.set("avatarUrl", httpResponse.data.avatarUrl);
+      return user.signUp(null);
+    }
+  }).then(function (user) {
+    res.success(user);
   }, function (httpResponse) {
     res.error(httpResponse)
     console.error(`{"status":"${httpResponse.status}","error":"${httpResponse.error}"}`);
