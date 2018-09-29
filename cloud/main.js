a9f96b8e0c8fc9351e7571c73623dbd72a2739fb
walxlgf <guanghui-li@163.com>
2018-09-29 10:12:49 +0800

    注册时新建对应的role（用于共享) （验证登录错误8)

--- cloud/main.js
+++ cloud/main.js
@@ -1,14 +1,6 @@
-
 const APPID = 'wxc14d0ff891dbbb64';
 const SECRET = '654f6c6559336fa79d13c85e4cb2e080';
 
-Parse.Cloud.define('hello1', function (req, res) {
-  res.success("hi");
-});
-
-Parse.Cloud.define('hello2', (req, res) => {
-  res.success('ok');
-});
 
 
 Parse.Cloud.define('weappAuthOnlyCode', (req, res) => {
@@ -41,7 +33,6 @@ Parse.Cloud.define('weappAuthOnlyCode', (req, res) => {
       console.log(`cloud:weappAuthOnlyCode:login:`)
       return Parse.User.logIn(openid, openid);
     } else {
-      console.log(`cloud:weappAuthOnlyCode:signUp:`)
       isSignUp = true;
       //如果不存在 注册
       var user = new Parse.User();
@@ -50,26 +41,79 @@ Parse.Cloud.define('weappAuthOnlyCode', (req, res) => {
       return user.signUp(null);
     }
   }).then(function (user) {
-    console.log(`cloud:weappAuthOnlyCode:user:${user.get('username')}`);
-    // Parse.Cloud.startJob('signUpJob', { uesr });
-    singUpFuction(user);
+    //注册时 复制公共比赛和公共模板到当前用户下
+    if (isSignUp) {
+      //复制公共比赛
+      let PublicGame = Parse.Object.extend("PublicGame");
+      let query = new Parse.Query(PublicGame);
+      query.descending('_created_at');
+      query.find().then(function (pgames) {
+        console.log(`cloud:weappAuthOnlyCode:pgames:${pgames.length}`);
+        for (pgame of pgames) {
+          let Game = Parse.Object.extend("Game");
+          let game = new Game();
+          game.set('title', pgame.get('title'));
+          game.set('startChips', pgame.get('startChips'));
+          game.set('startTime', pgame.get('startTime'));
+          game.set('rounds', pgame.get('rounds'));
+          
+          let rebuy = pgame.get('rebuy');
+          game.set('rebuy', rebuy);
+          if (rebuy)
+            game.set('rebuyChips',  pgame.get('rebuyChips'));
+          let addon = pgame.get('addon');
+          game.set('addon', addon);
+          if (addon)
+            game.set('addonChips', pgame.get('addonChips'));
+
+          game.set('players', pgame.get('players'));
+          //设置Acl
+          let gameAcl = new Parse.ACL();
+          gameAcl.setPublicReadAccess(false);
+          gameAcl.setPublicWriteAccess(false);
+          gameAcl.setReadAccess(user.id, true);
+          gameAcl.setWriteAccess(user.id, true);
+          game.set('ACL', gameAcl);
+          game.save();
+        }
+      }, function (error) {
+        console.error(error);
+      });
+
+
+      //复制公共模板
+      let PublicPattern = Parse.Object.extend("PublicPattern");
+      query = new Parse.Query(PublicPattern);
+      query.descending('_created_at');
+      query.find().then(function (ppatterns) {
+        console.log(`cloud:weappAuthOnlyCode:ppatterns:${ppatterns.length}`);
+        for (ppattern of ppatterns) {
+          let Pattern = Parse.Object.extend("Pattern");
+          let pattern = new Pattern();
+          pattern.set('title', ppattern.get('title'));
+          pattern.set('rounds', ppattern.get('rounds'));
+          //设置Acl
+          let patternAcl = new Parse.ACL();
+          patternAcl.setPublicReadAccess(false);
+          patternAcl.setPublicWriteAccess(false);
+          patternAcl.setReadAccess(user.id, true);
+          patternAcl.setWriteAccess(user.id, true);
+          pattern.set('ACL', patternAcl);
+          pattern.save();
+        }
+      }, function (error) {
+        console.error(error);
+      });
+    }
+    console.log(`cloud:weappAuthOnlyCode:user:${JSON.stringify(user)}`);
     res.success(user);
-    // return user;
-    // console.log(`cloud:weappAuthOnlyCode:user1:${user1.get('username')}`);
-  }, function (error) {
-    res.error(error);
-    // console.error(`cloud:weappAuthOnlyCode:user:${user} error:${user}`);
+    console.log(`cloud:weappAuthOnlyCode:user1:${JSON.stringify(user)}`);
+  }, function (user, error) {
+    res.error(error)
+    console.error(`cloud:weappAuthOnlyCode:user:${user} error:${user}`);
   });
-  // .catch(() => {
-  //   response.error(`cloud:weappAuthOnlyCode:catch`);
-  // });
 
 });
-
-function singUpFuction(user) {
-  console.log(`cloud:singUpFuction:user:${user}`);
-}
-
 
 Parse.Cloud.define('weappauth', (req, res) => {
   console.log(`cloud:weappauth:code:${req.params.code}`)
